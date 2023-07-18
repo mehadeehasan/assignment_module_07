f154d14650abb3d78f5bc52033028cec76ec3e17
Md. Mehadee Hasan <69212670+mehadeehasan@users.noreply.github.com>
2023-07-18 23:49:01 +0600

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Todo from './components/todo.jsx'
+import './App.css'
+
+function App() {
+  return (
+    <>      
+      <Todo />
+    </>
+  )
+}
+export default App;
